a76f19e1f0b11dc1c2b45aa15b2cce4d18c00ee4
Thomas <tho.giotti@gmail.com>
2019-09-13 10:52:08 +0200

    init

---
+++ app/main.js
@@ -0,0 +1,22 @@
+import app from './app.js';
+
+import config from './config.js';
+
+// --------------------------------------------------------------------------------------------------------------------
+// INITIALISATION DE L'APPLICATION
+// --------------------------------------------------------------------------------------------------------------------
+
+function initializeRouter() {
+    // Instancier ici le Vanilla Router dans l'objet "app.mvc.router"
+    // ...
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// CODE PRINCIPAL
+// --------------------------------------------------------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialisation du routeur.
+    initializeRouter();
+});
